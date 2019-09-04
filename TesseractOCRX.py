7de79bab607ca0a106a426da5381fb32d2e9a31e
Otavio Rocha Faria <otavio-roch@hotmail.com>
2019-09-04 10:18:46 -0300

    10:48

--- TesseractOCRX.py
+++ TesseractOCRX.py
@@ -20,11 +20,11 @@ class TesseractOCR():
         conteudo = ""
         quant = len(os.listdir("imagens"));
 
-        for i in range(1,quant):
+        for i in range(quant):
             entrada = cv2.imread(path_img + str(i) + ".png")
-
+            fundo_branco = cv2.imread("fundo_branco.png")
             # amplia a imagem da placa em 4
-            img = cv2.resize(entrada, None, fx=4, fy=4, interpolation=cv2.INTER_CUBIC);
+            img = cv2.resize(entrada, None, fx=5, fy=5, interpolation=cv2.INTER_CUBIC);
             #cv2.imshow("ENTRADA", img)
 
             # Converte para escala de cinza
@@ -32,9 +32,14 @@ class TesseractOCR():
             # cv2.imshow("Escala Cinza", img)
 
             # Binariza imagem
-            ret, img = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
+            ret, img = cv2.threshold(img, 185, 255, cv2.THRESH_BINARY)
             #cv2.imshow("Limiar", img)
 
+            # Desfoque na Imagem
+            img = cv2.GaussianBlur(img, (5, 5), 0)
+            # cv2.imshow("Desfoque", img)
+
+            #Propiedade para diminuir um pouco da imagem
             (x, y, a, l) = cv2.boundingRect(img)
 
             x += 5
@@ -44,15 +49,11 @@ class TesseractOCR():
 
             img = img[y:y + l, x:x + a];
 
-            # Desfoque na Imagem
-            img = cv2.GaussianBlur(img, (5, 5), 0)
-            # cv2.imshow("Desfoque", img)
+            cv2.imwrite("saidas/" + str(i) + "-ocr.jpg", img)
 
-            cv2.imwrite("saidas/" + str(i) + "-ocr.png", img)
+            imagem = Image.open("saidas/" + str(i) + "-ocr.jpg");
 
-            imagem = Image.open("saidas/" + str(i) + "-ocr.png");
-
-            saida = pytesseract.image_to_string(imagem, lang='eng');
+            saida = pytesseract.image_to_string(imagem);
 
             conteudo += saida + " | ";
 
@@ -62,7 +63,6 @@ class TesseractOCR():
 
 
 TesseractOCR().leituraImg()
-
 
 
 
